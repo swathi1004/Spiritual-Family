63c2f65f32754ec29b2e504a018801309e0ff780
nagur-meeravali shaik <nagurmeeravali_shaik@srmap.edu.in>
2026-06-04 09:54:58 +0530

    spring initialized

---
+++ backend/src/test/java/com/spiritualfamily/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiritualfamily.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
